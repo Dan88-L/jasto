f26bc98bed02d44c256f1bd4122282c1a26135ea
limbudenil <limbudenil@gmail.com>
2018-10-29 21:46:59 +0000

    Action helper added

--- app/controllers/test-sharepoint.js
+++ app/controllers/test-sharepoint.js
@@ -2,9 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+
+    isExpanded: false,
+
+    
+
+
 	actions: {
-          onSelect(params){
-              alert(params);
-          }
-	}
+        onSelect(params){
+            alert(params);
+            alert(title);
+            alert(url);
+        },
+
+        expand: function() {
+            this.set('isExpanded', true);
+
+        },
+
+        contract: function() {
+            this.set('isExpanded', false);
+        },
+    }
+
+
+
+
 });
--- app/routes/test-sharepoint.js
+++ app/routes/test-sharepoint.js
@@ -1,10 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    expand: function() {
+      this.controller.set('isExpanded', true);
+    },
 
+    contract: function() {
+      this.controller.set('isExpanded', false);
+    }
+  },
 
+   model: function() {
+    return {
+      title: "Tomster",
+      url: "http://emberjs.com/images/about/ember-productivity-sm.png"};
+  },
 
-	model (){
-		return "denil";
-	}
 });
